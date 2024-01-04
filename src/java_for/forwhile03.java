5dba633b5c3eb79a96d65d3dbf0024e481ec61eb
Ray <RayWzm@163.com>
2024-01-04 20:22:34 +0800

    新增打印金字塔

--- src/java_for/forwhile03.java
+++ src/java_for/forwhile03.java
@@ -5,12 +5,19 @@
 
 package java_for;
 
+import java.util.Scanner;
+
 public class forwhile03 {
     //打印金字塔
     public static void main(String args[]) {
-        System.out.println(" * ");
-        for(int i=2;i<=6;i++){
-            for(int j=1;j<=i+1;j++){
+       // System.out.println(" * ");
+        Scanner scanner=new Scanner(System.in);
+        int num=scanner.nextInt();
+        for(int i=1;i<=num;i++){
+            for(int j=1;j<=num-i;j++){
+                System.out.printf(" ");
+            }
+            for(int j=1;j<=i*2-1;j++){
                 System.out.print("*");
             }
             System.out.println();
