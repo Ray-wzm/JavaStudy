e148e3d5c893d072b0c8dd5dfff37454ec9d907c
Ray <RayWzm@163.com>
2024-01-05 09:23:04 +0800

    新增打印金字塔”

--- src/java_for/forwhile03.java
+++ src/java_for/forwhile03.java
@@ -10,14 +10,16 @@ import java.util.Scanner;
 public class forwhile03 {
     //打印金字塔
     public static void main(String args[]) {
-       // System.out.println(" * ");
-        Scanner scanner=new Scanner(System.in);
-        int num=scanner.nextInt();
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("请输入要打印的金字塔的高度：");
+        int num = scanner.nextInt();
         for(int i=1;i<=num;i++){
+            //打印空格
             for(int j=1;j<=num-i;j++){
-                System.out.printf(" ");
+                System.out.print(" ");
             }
-            for(int j=1;j<=i*2-1;j++){
+            //打印实心金字塔
+            for(int j=1;j<=2*i-1;j++){
                 System.out.print("*");
             }
             System.out.println();
