52f555a14923e65e7d9aa85340fe3b5557d1a356
Ray <RayWzm@163.com>
2024-01-07 16:29:32 +0800

    数组练习题

--- src/java_array/array13.java
+++ src/java_array/array13.java
@@ -31,5 +31,11 @@ public class array13 {
             }
             System.out.println();
         }
+        String foo="blue";
+        boolean[] bar =new boolean[2];
+        if(bar[0]){
+            foo="green";
+        }
+        System.out.println(foo);
     }
 }
