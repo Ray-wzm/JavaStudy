fe15e2d660483a01614ccb393dae4e69149f3e6b
Ray <RayWzm@163.com>
2024-01-25 17:20:24 +0800

    动态绑定

---
+++ src/java_intermediate/com/dynamic/A.java
@@ -0,0 +1,22 @@
+/**
+ * @author: 一只羊驼
+ * @date: 2024/1/25
+ */
+
+package java_intermediate.com.dynamic;
+
+public class A {
+    public int i=10;
+
+    public int getI() {
+        return i;
+    }
+
+    public int sum(){
+        return getI()+10;
+    }
+
+    public int sum1(){
+        return i+10;
+    }
+}
---
+++ src/java_intermediate/com/dynamic/B.java
@@ -0,0 +1,19 @@
+/**
+ * @author: 一只羊驼
+ * @date: 2024/1/25
+ */
+
+package java_intermediate.com.dynamic;
+
+public class B extends A{
+    public int i =20;
+
+    @Override
+    public int getI() {
+        return i;
+    }
+
+    public int sum(){
+        return i+10;
+    }
+}
