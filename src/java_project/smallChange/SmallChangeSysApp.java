3ba9b8257925ceddfbb2c9d36a7c1b5060c620df
Ray <RayWzm@163.com>
2024-01-28 15:05:25 +0800

    零钱通项目-面向对象

---
+++ src/java_project/smallChange/SmallChangeSysApp.java
@@ -0,0 +1,15 @@
+/**
+ * @author 一只羊驼
+ * @date 2024/1/28
+ */
+
+package java_project.smallChange;
+
+import javax.swing.*;
+
+public class SmallChangeSysApp {
+    public static void main(String args[]) {
+        SmallChangeSysOOP oop = new SmallChangeSysOOP();
+        oop.mainMenu();
+    }
+}
