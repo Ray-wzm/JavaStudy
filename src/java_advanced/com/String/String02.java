ef3d18854f9e32cb83910210d1267c126de1a589
Ray <RayWzm@163.com>
2024-02-19 15:12:19 +0800

    String类

--- src/java_advanced/com/String/String02.java
+++ src/java_advanced/com/String/String02.java
@@ -10,5 +10,18 @@ public class String02 {
         //创建String对象有两种方式
         //1. 直接赋值
         //2. 调用构造器
+        Test1 ex = new Test1();
+        ex.change(ex.str,ex.ch);
+        System.out.print(ex.str+"and");
+        System.out.println(ex.ch);
+    }
+}
+
+class Test1{
+    String str = new String("hsp");
+    final char[] ch = {'j','a','v','a'};
+    public void change(String str,char ch[]){
+        str="java";
+        ch[0]='h';
     }
 }
