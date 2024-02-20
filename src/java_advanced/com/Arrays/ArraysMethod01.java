9b85c70f35c92ecb8cd2588180dca463e44eea95
Ray <RayWzm@163.com>
2024-02-20 17:31:46 +0800

    String\Array

---
+++ src/java_advanced/com/Arrays/ArraysMethod01.java
@@ -0,0 +1,32 @@
+/**
+ * @author: 一只羊驼
+ * @date: 2024/2/20
+ */
+
+package java_advanced.com.Arrays;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class ArraysMethod01 {
+    public static void main(String args[]) {
+        //Arrays里面包含了一系列静态方法，用于管理或操作数组（比如排序或搜索）
+        //(1)toString
+        Integer[] arr = {1, 2, 3};
+        System.out.println(Arrays.toString(arr));
+        //(2)sort
+        Integer[] integer = {-1,9,1,23,5};
+        //sort方法，可以通过传入一个接口 Comparator 实现定制排序
+        //传入两个参数，（1）排序的数组arr （2）实现了 Comparator 接口的匿名内部类，要求实现Compare方法
+
+        Arrays.sort(integer, new Comparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                Integer i1 = (Integer)o1;
+                Integer i2 =(Integer)o2;
+                return i1-i2;
+            }
+        });
+        System.out.println(Arrays.toString(integer));
+    }
+}
---
+++ src/java_advanced/com/String/StringBuilder01.java
@@ -0,0 +1,15 @@
+/**
+ * @author: 一只羊驼
+ * @date: 2024/2/20
+ */
+
+package java_advanced.com.String;
+
+public class StringBuilder01 {
+    //一个可变的字符序列。提供一个与StringBuffer兼容得API，不保证同步（可能存在多线程问题）；
+    //当字符串缓冲区被单个线程调用得时候，建议先采用该类
+    //StringBuilder主要操作是append和insert方法，可以重载这些方法，可以接收任意类型的数据
+    public static void main(String args[]) {
+        StringBuilder stringBuilder = new StringBuilder();
+    }
+}
